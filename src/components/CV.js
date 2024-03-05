a8ee61889afc9b645b849fcd430a7143c3a163f9
kevin729 <kevin@kevsbox.com>
2024-03-05 21:43:20 +0000

    Added mobile response

--- src/components/CV.js
+++ src/components/CV.js
@@ -4,7 +4,11 @@ import cv from './docs/cv.pdf'
 const CV = () => { return (
         <div>
             <h2>My CV</h2>
-            <embed src={cv} className="cv" />
+            
+
+            <object data={cv} className="cv">
+                <iframe src="https://docs.google.com/document/d/e/2PACX-1vQ1rpLjCph9RsZnpgKiM4yUhfbD1JOjwaK2JrAZhJ5WXteT87LAlRQyjBhBeFJWHSa_BEGFXgVofG57/pub?embedded=true" className="cv"></iframe>
+            </object>
         </div>
     )
 }
