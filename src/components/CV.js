e437fab70ac1bf2a237b7181f9daa0fddb068015
kevin729 <kevin@kevsbox.com>
2024-03-09 18:34:59 +0000

    animated certificate slide

--- src/components/CV.js
+++ src/components/CV.js
@@ -7,7 +7,7 @@ const CV = () => { return (
             
 
             <object data={cv} className="cv">
-                <iframe src="https://docs.google.com/document/d/e/2PACX-1vQ1rpLjCph9RsZnpgKiM4yUhfbD1JOjwaK2JrAZhJ5WXteT87LAlRQyjBhBeFJWHSa_BEGFXgVofG57/pub?embedded=true" className="cv"></iframe>
+            <iframe src="https://docs.google.com/document/d/e/2PACX-1vQ1rpLjCph9RsZnpgKiM4yUhfbD1JOjwaK2JrAZhJ5WXteT87LAlRQyjBhBeFJWHSa_BEGFXgVofG57/pub?embedded=true" className="cv"></iframe>
             </object>
         </div>
     )
--- src/components/Learning.js
+++ src/components/Learning.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import learning1 from "./images/learning1.png"
 import learning2 from "./images/learning2.png"
 import learning3 from "./images/learning3.png"
@@ -7,6 +7,7 @@ import { ArrowLeft } from "@mui/icons-material";
 import { ArrowRight } from "@mui/icons-material";
 
 var index = 0;
+var prevIndex = 0;
 const slideImages = [
     {
         image: learning1
@@ -22,38 +23,83 @@ const slideImages = [
     },
 ]
 
+var timer
+
 function next() {
+    prevIndex = index;
     if (index < slideImages.length-1) {
         index++
-        update()
+    } else {
+        index = 0
     }
-    
-    
+
+    update(false, false)
 }
 
 function prev() {
+    prevIndex = index;
     if (index > 0) {
         index--
-        update()
+        
+    } else {
+        index = slideImages.length-1
+    }
+
+    update(false, true)
+}
+
+function showSlide(cert, prev) {
+    if (prev) {
+        cert.className = 'cert cert-show-prev'
+    } else {
+        cert.className = 'cert cert-show'
+    }
+    
+}
+
+function clearSlide(oldCert, newCert, prev) {
+    if (prev) {
+        oldCert.className = 'cert cert-invisible-prev'
+    } else {
+        oldCert.className = 'cert cert-invisible'
+    }
+    
+    oldCert.addEventListener('animationend', function eventHandlier(e) { 
+        oldCert.className = 'cert cert-none' 
+        showSlide(newCert, prev)
+        oldCert.removeEventListener('animationend', eventHandlier)
+    })
+}
+
+function update(start, prev) {
+    clearInterval(timer)
+    timer = setInterval(next, 3000)
+
+    let certs = document.getElementsByClassName("cert")
+
+    for (let i = 0; i < certs.length; i++) {
+        if (start) {
+            showSlide(certs[index])
+        } else if (i == prevIndex) {
+            clearSlide(certs[prevIndex], certs[index], prev)
+        }
     }
 }
 
-function update() {
-    document.getElementById('cert').src = slideImages[index].image
-}
-
 const Learning = () => {
-    return (
+    return (        
         <div>
             {
-                // slideImages.map((image, key) => (
-
-                //     <img key={key} className="cert" src={learning1} style={style}/>        
-
-                // ))
+                useEffect(() => {
+                    update(true)
+                })
             }
             <div>
-                <img id="cert" className="cert" src={slideImages[index].image} />
+                {
+                    slideImages.map((val, key) => (
+                        <img key={key} className="cert cert-none" src={val.image} />
+                    ))
+                }      
             </div>
             
 
@@ -64,9 +110,9 @@ const Learning = () => {
             <div className="inline-block" style={{width:"50px"}} onClick={next} >
                 <ArrowRight/>
             </div>
-            
         </div>
-    )
+        
+    )    
 }
 
 export default Learning;
